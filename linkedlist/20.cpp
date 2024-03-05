961811c45d938e270a098fc53ace36cacfb6025f
Pushkar Kumar <workwithgoodguypj@gmail.com>
2024-03-05 14:02:58 +0530

    sort a stack

---
+++ linkedlist/20.cpp
@@ -0,0 +1,55 @@
+/*linked list random node
+Input
+["Solution", "getRandom", "getRandom", "getRandom", "getRandom", "getRandom"]
+[[[1, 2, 3]], [], [], [], [], []]
+Output
+[null, 1, 3, 2, 2, 3]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.getRandom(); // return 1
+solution.getRandom(); // return 3
+solution.getRandom(); // return 2
+solution.getRandom(); // return 2
+solution.getRandom(); // return 3
+// getRandom() should return either 1, 2, or 3 randomly. Each element should have equal probability of returning.
+*/ 
+
+// approch 1
+/*
+ 
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ 
+class Solution {
+public:
+vector<int> arr;
+    Solution(ListNode* head) {
+        ListNode* temp = head;
+        while(temp!=NULL){
+            arr.push_back(temp->val);
+            temp = temp ->next;
+        }
+    }
+    
+    int getRandom() {
+        int n = arr.size();
+        int random_index = rand()%n;
+        return arr[random_index];
+
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
+
+*/
