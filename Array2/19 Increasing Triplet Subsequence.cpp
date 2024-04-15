2cc748a86b1d827bfa1a6de28e5119d508ded8a2
GoodGuyPJ <workwithgoodguypj@gmail.com>
2024-04-15 22:43:53 +0530

    array questions

--- Array2/19 Increasing Triplet Subsequence.cpp
+++ Array2/19 Increasing Triplet Subsequence.cpp
@@ -5,6 +5,7 @@ public:
         int num1= INT_MAX;
         int num2= INT_MAX;
         for(int i=0; i<n; i++){
+            // nums1<nums2<nums2
             int num3= nums[i];
             if(num3<=num1){
                 num1= num3;
