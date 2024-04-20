92de384d17d24e86d1ed27115b5382c381eb3735
GoodGuyPJ <workwithgoodguypj@gmail.com>
2024-04-20 20:15:41 +0530

    Binary Search Questions

--- Binary_search/2 Implement Lower Bound.cpp
+++ Binary_search/2 Implement Lower Bound.cpp
@@ -1,15 +1,16 @@
-//Upper bound
-
-// lb= upper_bound(arr.begin(), arr.end(), x) - arr.begin(); -- get the index
+//lower bound
+// smaller index such that arr[ind] >= x
+Approch 1
+// lb= lower_bound(arr.begin(), arr.end(), x) - arr.begin(); -- get the index
 #include<bits/stdc++.h>
 using namespace std;
- 
+ approach 2
  int lowerbound(int arr[], int n, int x){
     int low = 0, high= n-1;
     int ans= n;
     while(low<=high){
         int mid = low+(high-low)/2;
-        if(arr[mid]>x){
+        if(arr[mid]>=x){
             ans = mid;
             high= mid-1;
         }
@@ -20,14 +21,12 @@ using namespace std;
     return ans;
  }
 int main(){
-    int arr[]= {2, 3, 6, 7, 8, 8, 11, 11, 11, 12};
+    int arr[]= {1, 2, 6, 6, 6, 6, 7};
     int n= sizeof(arr)/sizeof(int);
     int target;
     cin>>target;
 
     int lowerB= lowerbound(arr, n, target);
-
-    cout<<"array "<<arr[lowerB]<<endl;
-    cout<<"index "<<lowerB;
+    cout<<lowerB;
   return 0;
 }
--- Binary_search/3 Implement Upper Bound.cpp
+++ Binary_search/3 Implement Upper Bound.cpp
@@ -1,6 +1,6 @@
-//lower bound
-
-// lb= lower_bound(arr.begin(), arr.end(), x) - arr.begin(); -- get the index
+//Upper bound
+smallest index such that arr[ind] > x
+// lb= upper_bound(arr.begin(), arr.end(), x) - arr.begin(); -- get the index
 #include<bits/stdc++.h>
 using namespace std;
  
@@ -9,7 +9,7 @@ using namespace std;
     int ans= n;
     while(low<=high){
         int mid = low+(high-low)/2;
-        if(arr[mid]>=x){
+        if(arr[mid]>x){
             ans = mid;
             high= mid-1;
         }
@@ -20,12 +20,14 @@ using namespace std;
     return ans;
  }
 int main(){
-    int arr[]= {1, 2, 6, 6, 6, 6, 7};
+    int arr[]= {2, 3, 6, 7, 8, 8, 11, 11, 11, 12};
     int n= sizeof(arr)/sizeof(int);
     int target;
     cin>>target;
 
     int lowerB= lowerbound(arr, n, target);
-    cout<<lowerB;
+
+    cout<<"array "<<arr[lowerB]<<endl;
+    cout<<"index "<<lowerB;
   return 0;
 }
