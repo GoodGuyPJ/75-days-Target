01cecb0d9623a680f56a9500e049c38be809cb3a
GoodGuyPJ <workwithgoodguypj@gmail.com>
2024-04-25 22:44:52 +0530

    commit

---
+++ Practice.cpp
@@ -0,0 +1,24 @@
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+ class solution{
+    private:
+
+    public:
+    int numberofProvinces(vector<vector<int>> adj, int v){
+        vector<int> adjls[v];
+        for(int i=0; i<v; i++){
+            for(int i=0; i<v; i++){
+                if(adjls[i][j] == 1 )
+            }
+        }
+    }
+ }
+
+
+int main(){
+
+    
+  return 0;
+}
